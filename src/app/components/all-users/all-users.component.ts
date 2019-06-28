b43b4b0490c89e32be0c7cc07874f6542d9d5bd7
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2019-06-28 23:16:07 +0430

    Add loading spinner to all-users component

--- src/app/components/all-users/all-users.component.ts
+++ src/app/components/all-users/all-users.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {GuestService} from "../../services/guest/guest.service";
 import {User} from "../../services/models/user.model";
+import {UtilityService} from "../../services/utility.service";
 
 @Component({
   selector: 'app-all-users',
@@ -14,16 +15,20 @@ export class AllUsersComponent implements OnInit {
   public total: number;
   public itemsPerPage: number;
 
-  constructor(private guestService: GuestService) {
+  constructor(
+    private guestService: GuestService,
+    private utilityService: UtilityService) {
   }
 
   /**
    * ngOnInit life cycle hook
    */
   ngOnInit() {
+    this.utilityService.toggleLoadingSpinner('show');
     this.page = 1;
     this.guestService.getAllUsers(this.page).subscribe(data => {
       if (data) {
+        this.utilityService.toggleLoadingSpinner('hide');
         this.currentPage = data.current_page;
         this.total = data.total;
         this.itemsPerPage = data.per_page;
