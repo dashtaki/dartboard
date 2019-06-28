96f7a8c5d544f55dab38628513a9f965251cf241
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2019-06-29 03:13:25 +0430

    Add interceptors to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {GameComponent} from './components/game/game.component';
 import {GuestService} from './services/guest/guest.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AllUsersComponent} from './components/all-users/all-users.component';
 import {NgxPaginationModule} from 'ngx-pagination';
 import {LoginComponent} from './components/login/login.component';
@@ -13,6 +13,8 @@ import {UserService} from './services/user/user.service';
 import {UtilityService} from './services/utility.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RegisterComponent} from './components/register/register.component';
+import {ApiInterceptor} from './services/interceptors/api-interceptor.service';
+import {JwtInterceptor} from './services/interceptors/jwt-interceptor.service';
 
 const DECLARATIONS = [
   AppComponent,
@@ -32,7 +34,9 @@ const DECLARATIONS = [
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [GuestService, UserService, UtilityService],
+  providers: [GuestService, UserService, UtilityService,
+    {provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
