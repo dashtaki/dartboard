0b3ada310793e22aacdd1866d30b4eb3a434de28
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2019-06-28 23:14:48 +0430

    Add loading spinner

---
+++ src/app/services/utility.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilityService {
+
+  public toggleLoadingSpinner(mode: 'show' | 'hide') {
+    let spinner = document.querySelector('#loading-spinner');
+    if (mode === 'show')
+      spinner.classList.add('show');
+    else if (mode === 'hide')
+      spinner.classList.remove('show');
+  }
+}
