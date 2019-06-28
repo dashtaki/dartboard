17628e593f7aba9750e44a795f9c73d608bad2f1
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2019-06-29 03:14:51 +0430

    Check user is logged in or not on loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UtilityService} from "./services/utility.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dartboard';
+export class AppComponent implements OnInit {
+  public isUserLoggedIn: boolean;
+
+  constructor(private utilityService: UtilityService) {
+  }
+
+  ngOnInit(): void {
+    this.isUserLoggedIn = this.utilityService.isUserLoggedIn();
+  }
+
 }
