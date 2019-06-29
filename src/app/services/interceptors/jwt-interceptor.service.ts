83adae324930002a2e1512e07dbaadbd8095a19c
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2019-06-29 11:26:18 +0430

    Fix token property in jwt.interceptor

--- src/app/services/interceptors/jwt-interceptor.service.ts
+++ src/app/services/interceptors/jwt-interceptor.service.ts
@@ -6,10 +6,10 @@ import {Observable} from 'rxjs';
 export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let currentUser = JSON.parse(localStorage.getItem('userInfo'));
-    if (currentUser && currentUser.token) {
+    if (currentUser && currentUser.access_token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
+          Authorization: `Bearer ${currentUser.access_token}`
         }
       });
     }
