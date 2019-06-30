8aac9508d6bce8bacd69aac889d7abf696258b4b
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2019-07-01 02:02:56 +0430

    Unsubscribe all subscriptions in all-users component

--- src/app/components/all-users/all-users.component.ts
+++ src/app/components/all-users/all-users.component.ts
@@ -1,22 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GuestService} from '../../services/guest/guest.service';
 import {User} from '../../services/models/user.model';
 import {UtilityService} from '../../services/utility.service';
 import * as guestActions from '../../store/actions/guest.action';
 import * as fromRoot from '../../store/reducers';
 import {Store} from '@ngrx/store';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-all-users',
   templateUrl: './all-users.component.html',
   styleUrls: ['./all-users.component.scss']
 })
-export class AllUsersComponent implements OnInit {
+export class AllUsersComponent implements OnInit, OnDestroy {
   public users: User[];
   public page: number;
   public currentPage: number;
   public total: number;
   public itemsPerPage: number;
+  private subscription: Subscription;
 
   constructor(
     private guestService: GuestService,
@@ -33,7 +35,7 @@ export class AllUsersComponent implements OnInit {
     this.page = 1;
 
     this.store.dispatch(new guestActions.AllUsersAction(this.page));
-    this.store.select(fromRoot.getAllUsers).subscribe(data => {
+    this.subscription = this.store.select(fromRoot.getAllUsers).subscribe(data => {
       if (data) {
         this.currentPage = data.current_page;
         this.total = data.total;
@@ -42,6 +44,13 @@ export class AllUsersComponent implements OnInit {
         this.utilityService.toggleLoadingSpinner('hide');
       }
     });
+  }
+
+  /**
+   * ngOnDestroy life cycle hook
+   */
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
